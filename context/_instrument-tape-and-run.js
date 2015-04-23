e31698bb255838e8c5959725063b9c08b833332d
Raynos <raynos2@gmail.com>
2015-04-23 14:34:10 -0700

    import relative modules

--- context/_instrument-tape-and-run.js
+++ context/_instrument-tape-and-run.js
@@ -2,15 +2,21 @@
 
 /*global process*/
 var $process = process;
+var resolve = require('resolve');
+var path = require('path');
 
 var env = $process.env;
 var testProgram = env.ITAPE_NPM_TEST_PROGRAM;
+var baseDir = path.basename(testProgram);
 
 var leakedHandles = env.ITAPE_NPM_LEAKED_HANDLES;
 if (leakedHandles) {
     leakedHandles = JSON.parse(leakedHandles);
 
-    var leakedHandlesModule = require('leaked-handles');
+    var leakedHandlesFile = resolve.sync('leaked-handles', {
+        basedir: baseDir
+    });
+    var leakedHandlesModule = require(leakedHandlesFile);
     if (typeof leakedHandles === 'object') {
         leakedHandlesModule.set(leakedHandles);
     }
@@ -20,7 +26,10 @@ var formatStack = env.ITAPE_NPM_FORMAT_STACK;
 if (formatStack) {
     formatStack = JSON.parse(formatStack);
 
-    var formatStackModule = require('format-stack');
+    var formatStackFile = resolve.sync('format-stack', {
+        basedir: baseDir
+    });
+    var formatStackModule = require(formatStackFile);
     if (typeof formatStack === 'object') {
         formatStackModule.set(formatStack);
     } else {
@@ -30,7 +39,6 @@ if (formatStack) {
     }
 }
 
-var resolve = require('resolve');
 var whiteList = env.ITAPE_NPM_TAPE_WHITELIST;
 if (whiteList) {
     whiteList = JSON.parse(whiteList);
@@ -41,7 +49,7 @@ require(testProgram);
 
 function instrumentTape(whiteList) {
     var tapeTestFile = resolve.sync('tape/lib/test', {
-        basedir: process.cwd()
+        basedir: baseDir
     });
     var tapeTest = require(tapeTestFile);
 
